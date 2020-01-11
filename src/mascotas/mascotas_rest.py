0c1faa5cb5e59fefaa1548612171498a8610a727
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-01-11 12:37:23 +0100

    Integrada la búsqueda como un método más de la clase fixed #67

--- src/mascotas/mascotas_rest.py
+++ src/mascotas/mascotas_rest.py
@@ -6,17 +6,17 @@ mascotas de la clase "Mascotas".
 
 @author: Lidia Sánchez Mérida
 """
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask_caching import Cache
 import json
 import mascotas
 import sys
 sys.path.append("src")
-from excepciones import EmptyCollection, WrongPetIndex
+from excepciones import EmptyCollection, WrongPetIndex, WrongNumberSearchParameters, WrongSearchParametersValues, PetsNotFound, MaxPetfinderRequestsExceeded
+
 import os
 import mongodb
 
-"""Configuramos Flask para que pueda conectarse con el microservicio de celery"""
 app = Flask(__name__)
 """Añadimos caché para comprobar si mejoran las prestaciones."""
 app.config["CACHE_DEFAULT_TIMEOUT"] = 36000
@@ -46,15 +46,6 @@ def obtener_mascotas():
         return Response(json.dumps(mascotas), status=200, mimetype="application/json")
     except EmptyCollection:
         return Response("REST: No existen datos de mascotas aún.", status=400)
-#    try:
-#        tarea = descargar_mascotas.apply_async()
-#        mascotas = tarea.get()
-#        if (mascotas == None):
-#            return Response("No existen datos de mascotas aún. Espere....", status=200)
-#        else:
-#            return Response(json.dumps(mascotas), status=200, mimetype="application/json")
-#    except MaxPetfinderRequestsExceeded: 
-#        return Response("Gunicorn: Número de peticiones máximo excedido.", status=400)
 
 @app.route("/obtener_una_mascota/<string:id_mascota>", methods=['GET'])
 @cache.cached()
@@ -72,3 +63,36 @@ def obtener_una_mascota(id_mascota):
         return Response("REST: No se han recopilado datos de mascotas aún.", status=400)
     except WrongPetIndex:
         return Response("REST: No existe una mascota con el índice especificado.", status=404)
+    
+@app.route("/buscar_mascotas", methods=['GET'])
+def buscar_mascotas():
+    """Servicio REST con el que buscar mascotas en función de una serie de parámetros
+    configurables por el usuario."""
+    try:
+        terminos_busqueda = request.args
+        tipo_animal = edad = genero = tamanio = ninios = gatos = perros = ''
+        """Comprobamos los parámetros que se han pasado en la URL"""
+        if ('tipo_animal' in terminos_busqueda): tipo_animal = terminos_busqueda['tipo_animal']
+        if ('edad' in terminos_busqueda): edad = terminos_busqueda['edad']
+        if ('genero' in terminos_busqueda): genero = terminos_busqueda['genero']
+        if ('tamanio' in terminos_busqueda): tamanio = terminos_busqueda['tamanio']
+        if ('ninios' in terminos_busqueda): ninios = terminos_busqueda['ninios']
+        if ('gatos' in terminos_busqueda): gatos = terminos_busqueda['gatos']
+        if ('perros' in terminos_busqueda): perros = terminos_busqueda['perros']
+
+        """Preparamos el diccionario con la búsqueda."""
+        parametros = {'tipo_animal':tipo_animal, 'edad':edad, 'genero':genero,
+              'tamanio':tamanio, 'ninios':ninios, 'gatos':gatos, 'perros':perros}
+        mascotas_coincidentes = m.buscar(parametros)
+        return Response(json.dumps(mascotas_coincidentes), status=200, mimetype="application/json")
+    
+    except WrongNumberSearchParameters:
+        return Response("Número de parámetros de búsqueda incorrecto.", status=400)
+    except WrongSearchParametersValues:
+        return Response("Al menos un parámetro de búsqueda debe tener un valor.", status=400)
+    except PetsNotFound:
+        return Response("No existen mascotas sobre las que realizar la búsqueda.", status=428)
+    except TypeError:
+        return Response("Los valores de los parámetros no son válidos.", status=400)
+    except MaxPetfinderRequestsExceeded: 
+        return Response("Número de peticiones máximo excedido.", status=412)
